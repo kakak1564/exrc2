c862b59974deda7935981fc7ee9569c21ba076ea
sophieanjs <carvalho.sophia1406@gmail.com>
2024-10-17 13:47:30 -0300

    exrc2JS

---
+++ atv1.js
@@ -0,0 +1,11 @@
+let idade = 65
+
+if (idade< 60 && idade < 60) {
+    console.log("Adulto");
+}
+else if (idade >=60) {
+    console.log("Idoso");
+}
+else{
+    console.log("Menor de Idade");
+}
---
+++ atv4.js
@@ -0,0 +1,10 @@
+let peso =  70 
+let altura =1.80
+let imc = peso /(altura * altura)
+
+if (imc > 18.5 && imc < 24.9) {
+    console.log("Está Saudavel ");
+}
+else{
+    console.log("Não está Saudavel");
+}
